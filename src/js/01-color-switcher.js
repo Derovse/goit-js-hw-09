f7c647c55dbfa5e625eb120fbde358b4641121eb
Derovse <sashamartunyk2004@gmail.com>
2023-05-25 13:25:52 +0300

    ва
    в

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, '0')}`;
+}
+function changeBackgroundColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+stopBtn.disabled = true;
+let intervalId = null;
+
+function stopColorChanging() {
+  clearInterval(intervalId);
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+}
+
+function startColorChanging() {
+  clearInterval(intervalId);
+  intervalId = setInterval(changeBackgroundColor, 1000);
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+}
+stopBtn.addEventListener('click', stopColorChanging);
+startBtn.addEventListener('click', startColorChanging);
