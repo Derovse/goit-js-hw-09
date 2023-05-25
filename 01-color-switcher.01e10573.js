cf59152d5d1a2c26a21c79145cd50c3706b88ccd
Derovse <118081828+Derovse@users.noreply.github.com>
2023-05-25 10:30:09 +0000

    Deploying to gh-pages from @ Derovse/goit-js-hw-09@b8c567e0e43272d96b1a0f9f39792967fc2defb0 🚀

---
+++ 01-color-switcher.01e10573.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");function a(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"))}e.disabled=!0;var n=null;e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1,e.disabled=!0})),t.addEventListener("click",(function(){clearInterval(n),n=setInterval(a,1e3),t.disabled=!0,e.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.01e10573.js.map
---
+++ 01-color-switcher.a8aa6af2.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");function a(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,"0")}`}e.disabled=!0;let d=null;e.addEventListener("click",(function(){clearInterval(d),t.disabled=!1,e.disabled=!0})),t.addEventListener("click",(function(){clearInterval(d),d=setInterval(a,1e3),t.disabled=!0,e.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.a8aa6af2.js.map
